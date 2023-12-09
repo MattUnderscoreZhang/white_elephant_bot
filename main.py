53106eec4400c2866ae6964e339ced2cd1f05413
Matt Zhang <set.stun@gmail.com>
2023-12-08 20:09:56 -0500

    Attempting ping response

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ from white_elephant_bot.validation import validate_request
 app = FastAPI()
 
 
-@app.post("/")
-async def _(request: Request, response: Response):
-    if not validate_request(request):
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return "Invalid request signature"
-
-    if request["type"] == 1:
-        return {
-            "type": 1
-        }
+def ping_response():
+    return {
+        "type": 1
+    }
 
 
 async def test_key():
     test_key = os.getenv("TEST_KEY")
     return {"message": test_key}
+
+
+@app.post("/")
+async def _(request: Request, response: Response):
+    if not validate_request(request):
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return "Invalid request signature"
+    request_body = await request.json()
+    if request_body["type"] == 1:
+        return ping_response()
