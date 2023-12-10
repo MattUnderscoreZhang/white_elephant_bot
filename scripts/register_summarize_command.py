cec4a4561ec644900542dee157cdeeb337b81839
Matt Zhang <set.stun@gmail.com>
2023-12-09 22:57:54 -0500

    Added registration script for summarize command

---
+++ scripts/register_summarize_command.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+import requests
+from typing import cast
+
+
+load_dotenv()
+bot_token=cast(str, os.getenv("BOT_TOKEN"))
+bot_id=cast(str, os.getenv("BOT_ID"))
+guild_id=cast(str, os.getenv("GUILD_ID"))
+
+
+url = f"https://discord.com/api/v10/applications/{bot_id}/guilds/{guild_id}/commands"
+
+
+response = requests.post(
+    url,
+    headers={
+        "Authorization": f"Bot {bot_token}"
+    },
+    json={
+        "name": "summarize",
+        "description": "Summarize recent messages",
+        "type": 1,  # slash command
+        "options": [],
+    },
+)
+print(response.json())
